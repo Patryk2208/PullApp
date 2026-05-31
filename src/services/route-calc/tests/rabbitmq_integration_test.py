18c508d1d451fcb9c52261ee344c87dafd373a69
Wiktor Murawski <wiktor0204@gmail.com>
2026-05-31 12:24:54 +0200

    all tests passing

--- src/services/route-calc/tests/rabbitmq_integration_test.py
+++ src/services/route-calc/tests/rabbitmq_integration_test.py
@@ -22,7 +22,7 @@ from typing import Optional
 
 import pytest
 import aio_pika
-
+import aio_pika.abc
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -59,7 +59,7 @@ class SimpleJobMessage:
         }).encode()
 
 
-async def connect_to_rabbitmq() -> tuple[aio_pika.RobustConnection, aio_pika.AbstractRobustChannel]:
+async def connect_to_rabbitmq() -> tuple[aio_pika.RobustConnection, aio_pika.abc.AbstractRobustChannel]:
     """
     Connect to RabbitMQ
 
@@ -76,7 +76,7 @@ async def connect_to_rabbitmq() -> tuple[aio_pika.RobustConnection, aio_pika.Abs
     return connection, channel
 
 
-async def declare_queues(channel: aio_pika.AbstractRobustChannel):
+async def declare_queues(channel: aio_pika.abc.AbstractRobustChannel):
     """Declare compute and results queues"""
 
     compute_queue = await channel.declare_queue(
